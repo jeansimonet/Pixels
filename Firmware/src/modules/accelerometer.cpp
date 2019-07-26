e0344626883352ec171255ee611333911e4a098f
Jean Simonet <frogie@gmail.com>
2019-07-26 16:37:01 -0400

    Small typo caused incorrect debug logs.

--- Firmware/src/modules/accelerometer.cpp
+++ Firmware/src/modules/accelerometer.cpp
@@ -141,7 +141,7 @@ namespace Accelerometer
 
         if (newRollState != rollState) {
             rollState = newRollState;
-            NRF_LOG_INFO("State: %d, Face %d", rollState, currentFace);
+            NRF_LOG_INFO("State: %d, Face %d", rollState, currentFace());
 
 			// Notify clients
 			for (int i = 0; i < rollStateClients.Count(); ++i)
