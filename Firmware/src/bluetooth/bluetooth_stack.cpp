7915e2013decf18e025952898573f19c1a0d7a77
Jean Simonet <frogie@gmail.com>
2020-01-26 11:47:22 -0500

    Reduce BLE connection interval to improve update rate

--- Firmware/src/bluetooth/bluetooth_stack.cpp
+++ Firmware/src/bluetooth/bluetooth_stack.cpp
@@ -34,8 +34,8 @@ namespace Stack
     #define APP_BLE_OBSERVER_PRIO           3                                       /**< Application's BLE observer priority. You shouldn't need to modify this value. */
     #define APP_BLE_CONN_CFG_TAG            1                                       /**< A tag identifying the SoftDevice BLE configuration. */
 
-    #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)        /**< Minimum acceptable connection interval (0.1 seconds). */
-    #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(200, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (0.2 second). */
+    #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(10, UNIT_1_25_MS)        /**< Minimum acceptable connection interval (0.01 seconds). */
+    #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(10, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (0.02 second). */
     #define SLAVE_LATENCY                   0                                       /**< Slave latency. */
     #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)         /**< Connection supervisory timeout (4 seconds). */
 
